f1fe1b633f5511a686c2cbb795f6fd2d27194223
EduSUT <edusut.vssut@gmail.com>
2026-04-26 15:34:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 
 
 export const supabaseConfig = {
-  url: "https://your-project-url.supabase.co",
-  anonKey: "your-anon-key"
+  url: "https://hjxavmlhydhpotrplxfu.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqeGF2bWxoeWRocG90cnBseGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxOTA0NjIsImV4cCI6MjA5Mjc2NjQ2Mn0.o0p4ci_t3sSH_Dfh0iAykNjrX6NKzWlLSntA6IZLLO0"
 };

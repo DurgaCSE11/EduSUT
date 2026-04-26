d97acc1bca46a6660b0a72590abca6be5eb19c85
DURGA PRASAD MAHAPATRA <durgaprasadmahapatra21@gmail.com>
2026-04-26 14:14:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,3 @@
-// This file is in .gitignore and will not be uploaded to GitHub
-export const firebaseConfig = {
-  apiKey: "AIzaSyBuZruAk7KoodRRVRmL5o4C6usjCFh56xQ",
-  authDomain: "edusut-d2ed3.firebaseapp.com",
-  projectId: "edusut-d2ed3",
-  storageBucket: "edusut-d2ed3.firebasestorage.app",
-  messagingSenderId: "488727279181",
-  appId: "1:488727279181:web:3cf1eac8b9358369b60e04",
-  measurementId: "G-DPT7H9DMD8"
-};
-
 export const supabaseConfig = {
   url: "https://kyfppyarpyjvtpmnochw.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5ZnBweWFycHlqdnRwbW5vY2h3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODEyNTYsImV4cCI6MjA5Mjc1NzI1Nn0.WzpF7jvPX6X7ko4QZlTvtjVv6u5D1b_jvwcYlBFpyDM"
